abf804babcd2877ea147b0fadcebd47022af27c0
abeln94 <544125@unizar.es>
2019-10-17 23:42:59 +0200

    use templates and keywords

--- main/urls.py
+++ main/urls.py
@@ -4,10 +4,9 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
 
-    path('baja.html', TemplateView.as_view(template_name='baja.html'), name='baja'),
-    path('contacto.html', TemplateView.as_view(template_name='contacto.html'), name='contacto'),
-    path('index.html', TemplateView.as_view(template_name='index.html'), name='index'),
-    path('login.html', TemplateView.as_view(template_name='login.html'), name='login'),
-    path('logup.html', TemplateView.as_view(template_name='logup.html'), name='logup'),
-    path('mapa.html', TemplateView.as_view(template_name='mapa.html'), name='mapa'),
+    path('baja', TemplateView.as_view(template_name='baja.html'), name='baja'),
+    path('contacto', TemplateView.as_view(template_name='contacto.html'), name='contacto'),
+    path('login', TemplateView.as_view(template_name='login.html'), name='login'),
+    path('logup', TemplateView.as_view(template_name='logup.html'), name='logup'),
+    path('mapa', TemplateView.as_view(template_name='mapa.html'), name='mapa'),
 ]
--- sistInf/settings.py
+++ sistInf/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'estilos'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_URL = '/estilos/'
+STATIC_URL = '/static/'
