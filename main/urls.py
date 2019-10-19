696614d8e91aa73976abfca7b6ec1de9bfb9a66b
abeln94 <544125@unizar.es>
2019-10-19 12:25:51 +0200

    login/logout with django

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 
     path('baja', TemplateView.as_view(template_name='baja.html'), name='baja'),
     path('contacto', TemplateView.as_view(template_name='contacto.html'), name='contacto'),
-    path('login', TemplateView.as_view(template_name='login.html'), name='login'),
     path('logup', TemplateView.as_view(template_name='logup.html'), name='logup'),
     path('mapa', TemplateView.as_view(template_name='mapa.html'), name='mapa'),
+
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
