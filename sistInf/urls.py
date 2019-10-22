1928770418c5d1fb9d4d94f260eb83b2be6f88de
abeln94 <544125@unizar.es>
2019-10-22 11:08:36 +0200

    removed app, all in sistInf directory fixed login form

--- sistInf/urls.py
+++ sistInf/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', include('main.urls')),
     path('admin/', admin.site.urls),
+
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+
+    path('baja', TemplateView.as_view(template_name='baja.html'), name='baja'),
+    path('contacto', TemplateView.as_view(template_name='contacto.html'), name='contacto'),
+    path('logup', TemplateView.as_view(template_name='logup.html'), name='logup'),
+    path('mapa', TemplateView.as_view(template_name='mapa.html'), name='mapa'),
+
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
